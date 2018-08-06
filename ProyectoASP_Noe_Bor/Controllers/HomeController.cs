7ac52b2caf38fa9b3708c97c833ebaba980ffa43
mrbackslash <borjaruata@gmail.com>
2018-08-06 12:32:11 +0200

    Commit en master para añadir los cambios de la rama Borja

--- ProyectoASP_Noe_Bor/Controllers/HomeController.cs
+++ ProyectoASP_Noe_Bor/Controllers/HomeController.cs
@@ -8,13 +8,18 @@ using ProyectoASP_Noe_Bor.Models;
 
 namespace ProyectoASP_Noe_Bor.Controllers
 {
+    [Route("Home")]
     public class HomeController : Controller
     {
+        [Route("")]
+        [Route("index")]
+        [Route("~/")]
         public IActionResult Index()
         {
             return View();
         }
 
+        [Route("about")]
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
@@ -22,6 +27,7 @@ namespace ProyectoASP_Noe_Bor.Controllers
             return View();
         }
 
+        [Route("contact")]
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
@@ -29,6 +35,28 @@ namespace ProyectoASP_Noe_Bor.Controllers
             return View();
         }
 
+        [Route("articles")]
+        public IActionResult Articles()
+        {
+            ViewData["Message"] = "Your articles page.";
+
+            List<ArticleViewModel> articulosPrueba = new List<ArticleViewModel>()
+            {
+                new ArticleViewModel(1,"art01",4,4.80,"foto01"),
+                new ArticleViewModel(2,"art02",4,4.80,"foto02"),
+                new ArticleViewModel(3,"art03",4,4.80,"foto03"),
+                new ArticleViewModel(4,"art04",4,4.80,"foto04"),
+                new ArticleViewModel(5,"art05",4,4.80,"foto05"),
+                new ArticleViewModel(6,"art06",4,4.80,"foto06"),
+                new ArticleViewModel(7,"art07",4,4.80,"foto07"),
+                new ArticleViewModel(8,"art08",4,4.80,"foto08"),
+                new ArticleViewModel(9,"art09",4,4.80,"foto09")
+            };
+
+            return View(articulosPrueba);
+        }
+
+        [Route("error")]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
---
+++ ProyectoASP_Noe_Bor/Models/UserViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoASP_Noe_Bor.Models
+{
+    public class UserViewModel
+    {
+        public UserViewModel(string nickname, string nombre, string apellidos, string nIF, string mail, string contrasena)
+        {
+            Nickname = nickname;
+            Nombre = nombre;
+            Apellidos = apellidos;
+            NIF = nIF;
+            Mail = mail;
+            Contrasena = contrasena;
+        }
+
+        public int Id { get; }
+        public string Nickname { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string NIF { get; set; }
+        public string Mail { get; set; }
+        public string Contrasena { get; set; }
+        public bool Admin { get; set; }
+    }
+}
