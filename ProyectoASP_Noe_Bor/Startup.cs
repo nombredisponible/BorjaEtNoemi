5ed912ceb40ddc37f248107a9886817c2d9372fe
nombredisponible <thisismychurch@gmail.com>
2018-08-06 09:49:56 +0200

    Cambios de prueba en Noe_Branch

--- ProyectoASP_Noe_Bor/Startup.cs
+++ ProyectoASP_Noe_Bor/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+//cosas comentadas por noemi
 namespace ProyectoASP_Noe_Bor
 {
     public class Startup
