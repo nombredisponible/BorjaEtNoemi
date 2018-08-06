169fa1eb8bba791bd790828998801406d0b00772
mrbackslash <borjaruata@gmail.com>
2018-08-06 09:29:16 +0200

    Subir proyecto al git

---
+++ ProyectoASP_Noe_Bor/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProyectoASP_Noe_Bor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
